0c4a526b4f6abbef0ae2b29d43b2c74fcebae7cd
ayd0 <austindaytonogle@gmail.com>
2023-01-08 16:01:31 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const darkOverlay = document.querySelector('.dark-overlay');
+// const navHomeBtn = document.querySelector('#nav-home-btn');
+const rate = 2.0;
+
+darkOverlay.style.background = 'rgba(0,0,0,1)';
+
+document.addEventListener('scroll', () => {
+	let alpha = 85 / window.scrollY*rate;
+	if (alpha >= 0) {
+		darkOverlay.style.background = `rgba(0,0,0,${alpha})`;
+	}
+});
+
+/*
+	// default behavior w.o scrollspy
+navHomeBtn.addEventListener('click', () => {
+	darkOverlay.style.background = 'rgba(0,0,0,1)';
+});
+*/
