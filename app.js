cbb83b835aeb4a206c6082cf188142dab239d355
ayd0 <austindaytonogle@gmail.com>
2023-01-09 01:17:01 -0800

    added icon, new logos

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const darkOverlay = document.querySelector('.dark-overlay');
 // const navHomeBtn = document.querySelector('#nav-home-btn');
 
 const rate = 2.0;
+const bgColors = '0,0,0,';
 
-darkOverlay.style.background = 'rgba(0,0,0,1)';
+darkOverlay.style.background = `rgba(0,0,0,1)`;
 
 document.addEventListener('scroll', () => {
 	let alpha = 85 / window.scrollY*rate;
