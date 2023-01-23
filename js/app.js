e6cc5c37206dd29775148037b56bad94ffa809e6
ayd0 <austindaytonogle@gmail.com>
2023-01-22 19:13:17 -0800

    updated styling & code layout

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@ const darkOverlay = document.querySelector('.dark-overlay');
 const mainNavA = document.querySelector('#main-nav a');
 
 const rate = 2.0;
-const bgColors = '0,0,0,';
 
-darkOverlay.style.background = `rgba(0,0,0,1)`;
+darkOverlay.style.background = `rgb(13,25,31)`;
 
 document.addEventListener('scroll', () => {
 	let alpha = 85 / window.scrollY*rate;
 	if (alpha >= 0) {
-		darkOverlay.style.background = `rgba(0,0,0,${alpha})`;
+		darkOverlay.style.background = `rgba(13,25,31,${alpha})`;
 	}
 });
