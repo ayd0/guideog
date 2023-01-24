9cdac532285d19a952c3fb39a8e14ef34c9dd48c
ayd0 <austindaytonogle@gmail.com>
2023-01-23 22:26:38 -0800

    explore section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-const darkOverlay = document.querySelector('.dark-overlay');
+const mainOverlay = document.querySelector('.main-overlay');
 const mainNavA = document.querySelector('#main-nav a');
 
 const rate = 2.0;
 
-darkOverlay.style.background = `rgb(13,25,31)`;
+mainOverlay.style.background = `rgb(13,25,31)`;
 
 document.addEventListener('scroll', () => {
 	let alpha = 85 / window.scrollY*rate;
 	if (alpha >= 0) {
-		darkOverlay.style.background = `rgba(13,25,31,${alpha})`;
+		mainOverlay.style.background = `rgba(13,25,31,${alpha})`;
 	}
 });
