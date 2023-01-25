95c4c8126e833d24f23b24ea242e25bd2ba4f4da
ayd0 <austindaytonogle@gmail.com>
2023-01-24 19:50:21 -0800

    handle explore button clicks to set active

--- js/app.js
+++ js/app.js
@@ -1,15 +1,40 @@
 const mainOverlay = document.querySelector('.main-overlay');
-const mainNavA = document.querySelector('#main-nav a');
+const exploreCategories = document.querySelectorAll('#categories .nav-link');
+const exploreGuides = document.querySelectorAll('#guides .nav-link');
 
 const rate = 2.0;
 
+// Set initial background color/opacity
 mainOverlay.style.background = `rgb(18,35,43)`;
 // 13, 25, 31 -- original color
 // 25, 47, 59
 
+// Change background opacity on scroll
 document.addEventListener('scroll', () => {
 	let alpha = 85 / window.scrollY*rate;
 	if (alpha >= 0) {
 		mainOverlay.style.background = `rgba(18,35,43,${alpha})`;
 	}
 });
+
+// Handle explon button clicks
+let exploreBtnClickHandler = function(button, buttonGroup) {	
+	for (node of buttonGroup) {
+		node.classList.remove('custom-active');
+	}
+	button.classList.add('custom-active');
+}
+
+let categoryBtnClickHandler = function() {
+	exploreBtnClickHandler(this, exploreCategories);
+}
+
+let guideBtnClickHandler = function() {
+	exploreBtnClickHandler(this, exploreGuides);
+}
+
+for (let i = 0; i < 3; ++i) {
+	exploreCategories[i].addEventListener('click', categoryBtnClickHandler);
+	exploreGuides[i].addEventListener('click', guideBtnClickHandler);
+}
+
