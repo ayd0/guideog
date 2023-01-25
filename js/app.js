bfcd500d8cc03975eb4fb1ff445faf34a6d84c92
ayd0 <austindaytonogle@gmail.com>
2023-01-24 23:58:17 -0800

    finished explore button handling

--- js/app.js
+++ js/app.js
@@ -1,8 +1,70 @@
 const mainOverlay = document.querySelector('.main-overlay');
 const exploreCategories = document.querySelectorAll('#categories .nav-link');
 const exploreGuides = document.querySelectorAll('#guides .nav-link');
+const categoryAnchors = document.querySelectorAll('#categories-sub a');
+const guideAnchors = document.querySelectorAll('#guides-sub a');
 
 const rate = 2.0;
+
+let categoryNames = [
+	[
+		'c/ Programming',
+		'c/ Cooking',
+		'c/ Gardening',
+		'c/ Car Maintenence',
+		'c/ Travel',
+		'c/ Home Decorating',
+		'c/ Game Design'
+	],
+	[
+		'c/ Some Category',
+		'c/ Some Category',
+		'c/ Some Category',
+		'c/ Some Category',
+		'c/ Some Category',
+		'c/ Some Category',
+		'c/ Some Category'
+	],
+	[
+		'c/ Some Other Category',
+		'c/ Some Other Category',
+		'c/ Some Other Category',
+		'c/ Some Other Category',
+		'c/ Some Other Category',
+		'c/ Some Other Category',
+		'c/ Some Other Category'
+	]
+];
+
+let guideNames = [
+	[
+		'g/ Bootstrap: Mastering Glid & Flexbox',
+		'g/ Making Ramsay\'s burger',
+		'g/ Strike the Earth: Grow Some Potatoes',
+		'g/ How to Change Your Oil',
+		'g/ Must See Spots in Seoul',
+		'g/ Making your Home Offfice a Vivarium',
+		'g/ Unity for Winners'
+	],
+	[
+		'c/ Some Guide',
+		'c/ Some Guide',
+		'c/ Some Guide',
+		'c/ Some Guide',
+		'c/ Some Guide',
+		'c/ Some Guide',
+		'c/ Some Guide'
+	],
+	[
+		'c/ Some Other Guide',
+		'c/ Some Other Guide',
+		'c/ Some Other Guide',
+		'c/ Some Other Guide',
+		'c/ Some Other Guide',
+		'c/ Some Other Guide',
+		'c/ Some Other Guide'
+	]
+];
 
 // Set initial background color/opacity
 mainOverlay.style.background = `rgb(18,35,43)`;
@@ -17,20 +79,28 @@ document.addEventListener('scroll', () => {
 	}
 });
 
-// Handle explon button clicks
-let exploreBtnClickHandler = function(button, buttonGroup) {	
+// Handle explore button clicks
+let exploreBtnClickHandler = function(button, buttonGroup, buttonAnchors, anchorNames) {	
 	for (node of buttonGroup) {
 		node.classList.remove('custom-active');
 	}
 	button.classList.add('custom-active');
+	
+	let arr = Array.prototype.slice.call(buttonGroup);
+	let i = arr.indexOf(button);
+	let j = 0;
+
+	for (anchor of buttonAnchors) {
+		anchor.innerText = anchorNames[i][j++];
+	}
 }
 
 let categoryBtnClickHandler = function() {
-	exploreBtnClickHandler(this, exploreCategories);
+	exploreBtnClickHandler(this, exploreCategories, categoryAnchors, categoryNames);
 }
 
 let guideBtnClickHandler = function() {
-	exploreBtnClickHandler(this, exploreGuides);
+	exploreBtnClickHandler(this, exploreGuides, guideAnchors, guideNames);
 }
 
 for (let i = 0; i < 3; ++i) {
